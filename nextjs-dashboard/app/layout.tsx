9f25819b974b0b5cd0b7459fa1a8624b86b62613
eduard <alejandroeduard2019@gmail.com>
2023-12-05 11:30:33 -0500

    first commit

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,19 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        <h1>Cristian David Trilleras</h1>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>
+          hecho por cristian t
+        </footer>
+        </body>
+    </html>
+  );
+}
